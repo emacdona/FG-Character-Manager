c666a9a4418ae8b9985ce87e30c013ca21f8fa3e
Ed MacDonald <emacdona@ed-macdonalds-macbook-pro.local>
2010-04-11 01:10:56 -0400

    Sizeable refactoring; I moved a bunch of classes into a more descriptive package.
    Also, I'm sucessfully unmarshalling the xml file into java objects! (See
    the main method)

--- src/main/java/net/edmacdonald/fantasyGrounds/App.java
+++ src/main/java/net/edmacdonald/fantasyGrounds/App.java
@@ -1,13 +1,34 @@
 package net.edmacdonald.fantasyGrounds;
 
+import net.edmacdonald.fantasyGrounds.character.fourthEdition.jpg.Root;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    private static Log log = LogFactory.getLog(App.class);
+
     public static void main( String[] args )
+            throws JAXBException
     {
-        System.out.println( "Hello World!" );
+        InputStream is = App.class.getResourceAsStream("/4E_JPG.xml");
+
+        JAXBContext jc = JAXBContext.newInstance("net.edmacdonald.fantasyGrounds.character.fourthEdition.jpg");
+        
+        Unmarshaller um = jc.createUnmarshaller();
+
+        Root r = (Root) um.unmarshal(is);
+
+        log.info(r.getCharacter().getName().getContent());
+
     }
 }
